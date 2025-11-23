5832591ad583fd7936fbfcebc4bbc5db4259cd6c
Nets Gomes <bartholamuseg@gmail.com>
2025-11-23 22:51:29 +0530

    edit app id & app certificate

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ var appID string
 var appCertificate string
 
 func init() {
-	os.Setenv("APP_ID", "41648987b4f940e38d137bd7119767f6")
-	os.Setenv("APP_CERTIFICATE", "c7683668881e411abc9e3ce3c3d5fa45")
+	os.Setenv("APP_ID", "ef3c4016ec5a4334917187ee49be4e7d")
+	os.Setenv("APP_CERTIFICATE", "deb2b763a19a445fb364c1185a392d94")
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file found")
